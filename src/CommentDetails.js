7c034eb7e37f68797e8c895293f383eca2eab875
Sherwin <sherwinlukes@gmail.com>
2020-03-19 22:19:54 +0500

    custom children component commit

--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { image } from "faker"
+
 
 const CommentDetails = props => {
     return (
         <div className="comment" >
             <a href="/" className="avatar">
-                <img alt="avatar" src={image.avatar()} />
+                <img alt="avatar" src={props.avatar} />
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
-                <div className="text">Nice post!</div>
+                <div className="text">{props.desc}</div>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { image } from "faker"
 import CommentDetails from './CommentDetails'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetails author="sam"/>
-            <CommentDetails author="alex"/>
-            <CommentDetails author="jones"/>
+
+            <ApprovalCard>
+                <CommentDetails
+                    author="sam"
+                    timeAgo="Today at 5:30PM"
+                    desc="text 1"
+                    avatar={image.avatar()} />
+            </ApprovalCard>
+
+
+            <CommentDetails
+                author="alex"
+                timeAgo="Today at 2:30AM"
+                desc="text 2"
+                avatar={image.avatar()} />
+            <CommentDetails
+                author="jones"
+                timeAgo="Today at 07:30PM"
+                desc="text 3"
+                avatar={image.avatar()} />
         </div>
     )
 }
