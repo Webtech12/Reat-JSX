abda875eb11fa700ef87d87132477014d4d2461e
Sherwin <sherwinlukes@gmail.com>
2020-03-19 23:03:26 +0500

    more custom child components commit

--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 const CommentDetails = props => {
     return (
         <div className="comment" >
--- src/index.js
+++ src/index.js
@@ -3,31 +3,57 @@ import ReactDOM from 'react-dom'
 import { image } from "faker"
 import CommentDetails from './CommentDetails'
 import ApprovalCard from './ApprovalCard'
+import Placeholder from './Placeholder'
 
 const App = () => {
     return (
-        <div className="ui container comments">
 
-            <ApprovalCard>
-                <CommentDetails
-                    author="sam"
-                    timeAgo="Today at 5:30PM"
-                    desc="text 1"
-                    avatar={image.avatar()} />
-            </ApprovalCard>
+        <div>
+            <Placeholder>
+                <div className="ui icon header">
+                    <i className="pdf file outline icon"></i>
+                No documents are listed for this customer.
+                </div>
+                <div className="ui primary button">Add Document</div>
+            </Placeholder>
+
+            <Placeholder>
+                <h4 className="ui header">INFO</h4>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            </Placeholder>
 
 
-            <CommentDetails
-                author="alex"
-                timeAgo="Today at 2:30AM"
-                desc="text 2"
-                avatar={image.avatar()} />
-            <CommentDetails
-                author="jones"
-                timeAgo="Today at 07:30PM"
-                desc="text 3"
-                avatar={image.avatar()} />
+
+            {/* <Message header="header txt" para="pp"></Message> */}
         </div>
+
+        // <div className="ui container comments">
+
+        //     <ApprovalCard>
+        //         <CommentDetails
+        //             author="sam"
+        //             timeAgo="Today at 5:30PM"
+        //             desc="text 1"
+        //             avatar={image.avatar()} />
+        //     </ApprovalCard>
+
+        //     <ApprovalCard>
+        //         <CommentDetails
+        //             author="alex"
+        //             timeAgo="Today at 2:30AM"
+        //             desc="text 2"
+        //             avatar={image.avatar()} />
+        //     </ApprovalCard>
+
+        //     <ApprovalCard>
+        //         <CommentDetails
+        //             author="jones"
+        //             timeAgo="Today at 07:30PM"
+        //             desc="text 3"
+        //             avatar={image.avatar()} />
+        //     </ApprovalCard>
+
+        // </div>
     )
 }
 
