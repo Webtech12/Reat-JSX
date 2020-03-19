48e1f7c8640a77a0f6dfab26f0cadf0c1adabaad
Sherwin <sherwinlukes@gmail.com>
2020-03-19 23:22:52 +0500

    final commit

--- src/index.js
+++ src/index.js
@@ -3,57 +3,37 @@ import ReactDOM from 'react-dom'
 import { image } from "faker"
 import CommentDetails from './CommentDetails'
 import ApprovalCard from './ApprovalCard'
-import Placeholder from './Placeholder'
+// import Placeholder from './Placeholder'
 
 const App = () => {
     return (
 
-        <div>
-            <Placeholder>
-                <div className="ui icon header">
-                    <i className="pdf file outline icon"></i>
-                No documents are listed for this customer.
-                </div>
-                <div className="ui primary button">Add Document</div>
-            </Placeholder>
+        <div className="ui container comments">
 
-            <Placeholder>
-                <h4 className="ui header">INFO</h4>
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-            </Placeholder>
+            <ApprovalCard>
+                <CommentDetails
+                    author="sam"
+                    timeAgo="Today at 5:30PM"
+                    desc="text 1"
+                    avatar={image.avatar()} />
+            </ApprovalCard>
 
+            <ApprovalCard>
+                <CommentDetails
+                    author="alex"
+                    timeAgo="Today at 2:30AM"
+                    desc="text 2"
+                    avatar={image.avatar()} />
+            </ApprovalCard>
 
-
-            {/* <Message header="header txt" para="pp"></Message> */}
+            <ApprovalCard>
+                <CommentDetails
+                    author="jones"
+                    timeAgo="Today at 07:30PM"
+                    desc="text 3"
+                    avatar={image.avatar()} />
+            </ApprovalCard>
         </div>
-
-        // <div className="ui container comments">
-
-        //     <ApprovalCard>
-        //         <CommentDetails
-        //             author="sam"
-        //             timeAgo="Today at 5:30PM"
-        //             desc="text 1"
-        //             avatar={image.avatar()} />
-        //     </ApprovalCard>
-
-        //     <ApprovalCard>
-        //         <CommentDetails
-        //             author="alex"
-        //             timeAgo="Today at 2:30AM"
-        //             desc="text 2"
-        //             avatar={image.avatar()} />
-        //     </ApprovalCard>
-
-        //     <ApprovalCard>
-        //         <CommentDetails
-        //             author="jones"
-        //             timeAgo="Today at 07:30PM"
-        //             desc="text 3"
-        //             avatar={image.avatar()} />
-        //     </ApprovalCard>
-
-        // </div>
     )
 }
 
